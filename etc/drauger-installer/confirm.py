f34ac327553b8435834ea6b405a89c90b5a8942c
Thomas Castleman <thomas.castleman2019@gmail.com>
2019-02-02 01:38:55 -0500

    begun change from zenity to python-gtk
    making python files for GTK window generation.

---
+++ etc/drauger-installer/confirm.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  error.py
+#  
+#  Copyright 2019 Thomas Castleman <draugeros@gmail.com>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk, Gdk
+from sys import argv
+
+info=argv[1]
+action=argv[2]
+
+class confirm(Gtk.Window):
+	def __init__(self):
+			Gtk.Window.__init__(self, title="Drauger Installer")
+			self.grid=Gtk.Grid(orientation=Gtk.Orientation.VERTICAL,)
+			self.add(self.grid)
+
+			self.label = Gtk.Label()
+			self.label.set_markup("""
+Package Info:
+
+	%s
+	
+Would you like to %s this package?
+	""" % (info, action))
+			self.label.set_justify(Gtk.Justification.CENTER)
+			self.grid.attach(self.label, 1, 1, 8, 1)
+			
+			self.button1 = Gtk.Button.new_with_label("YES")
+			self.button1.connect("clicked", self.onyesclicked)
+			self.grid.attach(self.button1, 7, 2, 1, 1)
+			
+			self.button1 = Gtk.Button.new_with_label("NO")
+			self.button1.connect("clicked", self.onnoclicked)
+			self.grid.attach(self.button1, 5, 2, 1, 1)
+			
+	def onyesclicked(self, button):
+		exit(0)
+		
+	def onnoclicked(self, button):
+		exit(1)
+
+def show_conf():
+	window = confirm()
+	window.set_decorated(True)
+	window.set_resizable(True)
+	window.override_background_color(Gtk.StateType.NORMAL, Gdk.RGBA(1,1,1,1))
+	window.modify_fg(Gtk.StateFlags.NORMAL, Gdk.color_parse("black"))
+	window.set_opacity(0.0)
+	window.set_position(Gtk.WindowPosition.CENTER)
+	window.show_all()
+	Gtk.main() 
+	window.connect("delete-event", Gtk.main_quit)
+
+show_conf()
