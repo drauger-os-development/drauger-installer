181933e2b7a87f730414bdcc784cd5d900cd4f13
Thomas Castleman <thomas.castleman2019@gmail.com>
2019-02-03 17:51:24 -0500

    created file selection dialoge
    Because of the way it works, it is built into the splash.py file. This makes it easier to handle the output. Also, Drauger Installer should still exit when you hit "Cancel"

--- etc/drauger-installer/splash.py
+++ etc/drauger-installer/splash.py
@@ -42,8 +42,32 @@ class splash(Gtk.Window):
 			self.button1.connect("clicked", self.onnextclicked)
 			self.grid.attach(self.button1, 7, 2, 1, 1)
 		
-		def onnextclicked(self, button):
-			exit(0)
+		def onnextclicked(self, widget):
+			dialog = Gtk.FileChooserDialog("Please choose a file", self,
+			Gtk.FileChooserAction.OPEN,
+			(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+			Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+
+			self.add_filters(dialog)
+
+			response = dialog.run()
+			if response == Gtk.ResponseType.OK:
+				exit(dialog.get_filename())
+			elif response == Gtk.ResponseType.CANCEL:
+				exit(2)
+
+			dialog.destroy()
+			
+		def add_filters(self, dialog):
+			filter_text = Gtk.FileFilter()
+			filter_text.set_name("Archive")
+			filter_text.add_mime_type("application/vnd.debian.binary-package")
+			dialog.add_filter(filter_text)
+			
+			filter_any = Gtk.FileFilter()
+			filter_any.set_name("Any files")
+			filter_any.add_pattern("*")
+			dialog.add_filter(filter_any)
 
 
 
